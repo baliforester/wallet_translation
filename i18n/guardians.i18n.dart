73d6547e037b7cbc9763bb7fde9ba9aafe01370f
baliforester <78919860+baliforester@users.noreply.github.com>
2021-02-12 19:48:05 +0800

    Update guardians.i18n.dart

--- i18n/guardians.i18n.dart
+++ i18n/guardians.i18n.dart
@@ -3,16 +3,16 @@ import 'package:i18n_extension/i18n_extension.dart';
 extension Localization on String {
   static var _t = Translations.byLocale("en_us") +
       {
-        "es_es": {
-          'Select Guardian': 'Seleccionar Guardian',
-          'Next': 'Seguir',
-          'Choose existing Seeds Member to add as guardians': 'Elija miembros de Seeds para agregar como guardianes',
-          'Select up to 5 Guardians to invite': 'Selecciona hasta 5 guardianes para invitar ',
-          'Enter user name or account': "Ingresa nombre de cuenta",
-          'Invite Guardians': "Invitar Guardianes",
-          'Send Invite': "Enviar Invitacion",
-          'The users below will be sent an invite to become your Guardian.': "Los siguientes usuarios recibirán una invitación para convertirse en su Guardian.",
-          'Invites Sent!': "Invitaciones Enviadas",
+        "id_id": {
+          'Select Guardian': 'Pilih Wali',
+          'Next': 'Lanjut',
+          'Choose existing Seeds Members to add as guardians': 'Pilih Anggota Seeds untuk ditambahkan sebagai wali',
+          'Select up to 5 Guardians to invite': 'Pilih hingga 5 Wali untuk kirim invitasi',
+          'Enter username or account': "Masukkan nama pengguna atau akun",
+          'Invite Guardians': "Undang Wali",
+          'Send Invite': "Kirim Invitasi",
+          'The users below will be sent an invite to become your Guardian.': "Pengguna di bawah ini akan dikirimi invitasi untuk menjadi Wali Anda.",
+          'Invites Sent!': "Invitasi Terkirim!",
         }
       };
 
