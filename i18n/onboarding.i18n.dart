064b07a0c44d0130ed82c3917e7ccfa5b3dd1688
baliforester <78919860+baliforester@users.noreply.github.com>
2021-02-13 09:44:24 +0800

    Update onboarding.i18n.dart

--- i18n/onboarding.i18n.dart
+++ i18n/onboarding.i18n.dart
@@ -4,22 +4,22 @@ extension Localization on String {
 
 static var _t = Translations.byLocale("en_us") +
     {
-      "es_es": {
+      "id_id": {
         
         // TRANSLATION NOTE: These are automatic translations by Google Translate - please replace
 
-        'Better than free transactions': 'Transacciones mejor que gratis',
+        'Better than free transactions': 'Transaksi lebih baik dari gratis',
         "Make payments globally without any fees. Earn rewards when you support 'regenerative' organisations and people.":
-        "Realiza pagos a nivel mundial sin ningún cargo. Obtenga recompensas cuando apoye a organizaciones y personas 'regenerativas'.",
+        "Lakukan pembayaran secara global tanpa biaya apa pun. Dapatkan imbalan jika Anda mendukung organisasi dan orang 'regeneratif'.",
         'Vote directly on social and environmental impact projects you care about.':
-        'Vota directamente sobre los proyectos de impacto social y ambiental que te interesan.',
-        'Citizen Campaigns': 'Campañas ciudadanas',
+        'Memilih secara langsung pada proyek-proyek dampak sosial dan lingkungan yang Anda pedulikan.',
+        'Citizen Campaigns': 'Kampanye Warga',
         'Unite with a global movement of organisations and people to regenerate our planet and heal our economy.': 
-        'Únete a un movimiento global de organizaciones y personas para regenerar nuestro planeta y sanar nuestra economía.',
-        'Regenerative Economy': 'Economía regenerativa',
-        'NEXT': 'SIGUIENTE',
-        'BACK': 'ATRÁS',
-        'JOIN NOW': 'ÚNETE AHORA',
+        'Bersatu dengan gerakan global organisasi dan orang-orang untuk meregenerasi dunia kita dan memulihkan ekonomi kita.',
+        'Regenerative Economy': 'Ekonomi Regeneratif',
+        'NEXT': 'LANJUT',
+        'BACK': 'KEMBALI',
+        'JOIN NOW': 'BERGABUNG SEKARANG',
       }
     };
 
