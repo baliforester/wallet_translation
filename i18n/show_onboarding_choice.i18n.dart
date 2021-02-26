2801a1c1237138f956c146ba3eff98e657cd6313
baliforester <78919860+baliforester@users.noreply.github.com>
2021-02-26 16:59:44 +0800

    Update show_onboarding_choice.i18n.dart

--- i18n/show_onboarding_choice.i18n.dart
+++ i18n/show_onboarding_choice.i18n.dart
@@ -4,16 +4,16 @@ extension Localization on String {
 
 static var _t = Translations.byLocale("en_us") +
     {
-      "es_es": {
-        'You can ask for an invite at': "Puedes solicitar una invitación en",
-        "Membership based on Web of Trust": "Membresía basada en una Red de Confianza",
-        "By signing up, you agree to our terms and privacy policy": "Al registrarte, aceptas nuestros términos y política de privacidad",
-        'Terms & Conditions': "Términos y condiciones",
-        'Privacy Policy': "Política de privacidad",
-        'If you have an account\nclick here': "Si tienes cuenta\ntoca aquí",
-        'Import private key': "Importar llave privada",
-        'If you have an invite\nclick here': "Si tienes invitación\ntoca aquí",
-        "Claim invite code": "Reclama código de invitación",
+      "id_id": {
+        'You can ask for an invite at': "Anda dapat meminta undangan di",
+        "Membership based on Web of Trust": "Keanggotaan berdasarkan kepercayaan",
+        "By signing up, you agree to our terms and privacy policy": "Dengan mendaftar, Anda menyetujui persyaratan dan kebijakan privasi kami",
+        'Terms & Conditions': "Syarat & Ketentuan",
+        'Privacy Policy': "Rahasia pribadi",
+        'If you have an account\nclick here': "Jika Anda memiliki akun \ klik di sini",
+        'Import private key': "Impor kunci pribadi",
+        'If you have an invite\nclick here': "Jika Anda memiliki undangan \ klik di sini",
+        "Claim invite code": "Klaim kode undangan",
       }
     };
 
